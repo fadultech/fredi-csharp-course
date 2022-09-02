2222fcc08776e22472f71612d50867cbfec17158
Jairo Fadul <jairo@fadul.tech>
2022-09-02 02:39:00 +0000

    Leitura-escrita na console

--- Program.cs
+++ Program.cs
@@ -4,47 +4,20 @@ namespace dotnetcore
 {
     class Program
     {
-        public int versao;
         static void Main(string[] args)
         {
-            // Tipagem de variáveis
-            int x = 10;
-            decimal d = 1.33M;  // Utilizado para cálculos financeiros (moeda)
-            double d2 = 1.33;   // Utilizado para cálculos científicos e medições
-            float f = 1.33F;    // Utilizado com engines de jogos devido à performance (Unity)
-            int y = x *2;
-            string s = "Olá";
-            s += " Mundo!";
+            Console.Write("Digite o seu nome: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Olá {name}!");
+            Console.Write("Digite o ano do seu nascimento: ");
+            string years = Console.ReadLine();
+            int year = int.Parse(years);
+            int age = 2022 - year;
+            Console.WriteLine($"Você tem {age} anos.");
 
-            // Usando vetor
-            int[] a = new int[] {2, 5, 7};
-            int result = 0;
-            foreach (int number in a)
-            {
-                result += number;
-            }
-
-            // Usando classes e objetos
-            Program p = new Program();
-            p.versao = 12;
-            Test t = new Test();
-            t.name = "Jairo";
-            string hello = t.SayHello();
-
-            // Imprimindo
-            Console.WriteLine(s);
-            Console.WriteLine(result);
-            Console.WriteLine(p.versao);
-            Console.WriteLine(hello);
 
         }
     }
 
-    class Test{
-        public string name;
-
-        public string SayHello(){
-            return "Olá " + name;
-        }
-    }
+   
 }
