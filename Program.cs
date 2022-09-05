c78ce2bc1cc2a6b9db74cd97f1b5eb4ea7c3c4ed
Jairo Fadul <jairo@fadul.tech>
2022-09-05 13:42:25 -0300

    Entradas e saidas na console

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
+            // entrada e saída na console
             Console.Write("Digite o seu nome: ");
             string name = Console.ReadLine();
             Console.WriteLine($"Olá {name}!");
@@ -15,9 +16,16 @@ namespace dotnetcore
             int age = 2022 - year;
             Console.WriteLine($"Você tem {age} anos.");
 
+            // desvio condicional
+            if (age >= 18) Console.Write("Você é maior de idade.");
+            else Console.Write("Você é menor de idade.");
 
+            // usando array
+            string[] names = { "Fredi", "Carlos" };
         }
-    }
 
-   
+    }
 }
+
+
+
