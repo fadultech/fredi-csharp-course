d5e85d1e9fd4803c6c79034da81943acf9bc93c9
Jairo Fadul <jairo@fadul.tech>
2022-09-08 13:42:44 -0300

    metodo Main com parametros

--- Program.cs
+++ Program.cs
@@ -4,24 +4,13 @@ namespace dotnetcore
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] v)
         {
-            // entrada e saída na console
-            Console.Write("Digite o seu nome: ");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Olá {name}!");
-            Console.Write("Digite o ano do seu nascimento: ");
-            string years = Console.ReadLine();
-            int year = int.Parse(years);
-            int age = 2022 - year;
-            Console.WriteLine($"Você tem {age} anos.");
-
-            // desvio condicional
-            if (age >= 18) Console.Write("Você é maior de idade.");
-            else Console.Write("Você é menor de idade.");
-
             // usando array
             string[] names = { "Fredi", "Carlos" };
+
+            Console.WriteLine(v[0]);
+            Console.WriteLine(v[1]);
         }
 
     }
